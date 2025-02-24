e3b7d6706e237ed7cfa55a9bf1d2075e351a96fe
Nisrine-C <nisrinechakir237@gmail.com>
2025-02-24 09:16:32 +0000

    created an implementation of IDao

---
+++ src/main/java/TP1/dao/DaoImpl.java
@@ -0,0 +1,11 @@
+package TP1.dao;
+
+public class DaoImpl implements IDao {
+
+    @Override
+    public double getData() {
+        System.out.println("Version base de donnees");
+        double temp = 23;
+        return temp;
+    }
+}
